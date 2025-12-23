f19bc27c717669c6d0126deaff43273aa6cf157a
Ahmet Can Koca <ahmetcankoca1990@hotmail.com>
2025-12-23 21:55:25 +0300

    feat: core urls and bootstrap download

--- sitem/urls.py
+++ sitem/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index"),
-]
+    path('', include('core.urls')),
+    ]
